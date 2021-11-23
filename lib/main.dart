5a4d55e45815505bc1a89af9d83bfea2125fbb2a
amitgain1818 <amitgain1818@gmail.com>
2021-11-23 16:44:36 +0600

    Amit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:daily_expenses/widgets/user_transaction.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Daily Shop',
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.cyan,
+          title: Center(
+            child: Text(
+              'Daily Expenses',
+            ),
+          ),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              width: double.infinity,
+              child: Card(
+                margin: EdgeInsets.all(10),
+                color: Colors.teal,
+                child: Center(
+                  child: Text(
+                    'Chart',
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            UserTrasaction(),
+          ],
+        ),
+      ),
+    );
+  }
+}
