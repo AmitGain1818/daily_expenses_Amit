27c5d426d917bf5aa95bfd9558f2ec1cf92fa87b
amitgain1818 <amitgain1818@gmail.com>
2021-12-02 17:01:33 +0600

    Remove

--- lib/main.dart
+++ lib/main.dart
@@ -39,12 +39,12 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final List<Transaction> _userTransactions = [
-    Transaction(
-      id: 't1',
-      title: 'iPhone',
-      amount: 1399,
-      date: DateTime.now(),
-    ),
+    // Transaction(
+    //   // id: 't1',
+    //   // title: 'iPhone',
+    //   // amount: 1399,
+    //   // date: DateTime.now(),
+    // ),
   ];
 
   void _addNewTransaction(String txTitle, double txAmount) {
